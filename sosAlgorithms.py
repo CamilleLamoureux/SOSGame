889fa25ec85db7235ad8b8d8619e3f6ec73470f4
Camille <291756@supinfo.com>
2018-12-23 17:32:55 +0100

    Structuration des bibliothèques
    Ajout de tous les commentaires et "def" permettant la structuration des documents

---
+++ sosAlgorithms.py
@@ -0,0 +1,26 @@
+# 2e projet de Python - SOS GAME (Groupe de 2)
+# Anaïs Depeau et Camille Lamoureux
+# Bibliothèque des fonctions algorithmiques permettant le jeu
+
+# Fonction de création d'un nouveau tableau
+def newboard(n):
+
+
+# Fonction qui check si la case sélectionnée par le joueur est correcte
+def possibleSquare(noard,n,i,j):
+
+
+# Procédure qui met à jour lines et scores si on a posé un S
+def updateScoreS(board,n,i,j,scores,player,lines):
+
+
+# Procédure qui met à jour lines et scores si on a posé un O
+def updateScoreO(board,n,i,j,scores,player,lines):
+
+
+# Prodécudre qui met à jour le plateau de jeu
+def update(board,n,i,j,l,scores,player,lines):
+
+
+# Fonction qui retourne le gagnant de la partie
+def winner(scores):
---
+++ sosGUI.py
@@ -0,0 +1,38 @@
+# 2e projet de Python - SOS GAME (Groupe de 2)
+# Anaïs Depeau et Camille Lamoureux
+# Bibliothèque des fonctions graphiques
+
+# Procédure qui dessine le tableau initial
+def drawBoard(mySurface):
+
+
+# Procédure qui affiche les scores des joueurs
+def displayScore(mySurface,n,scores):
+
+
+# Procédure qui permet d'afficher quel joueur doit jouer
+def displayPlayer(mySurface,n,player):
+
+
+# Procédure qui affiche la lettre posée par le joueur dont c'est le tour
+def drawCell(mySurface,board,i,j,player):
+
+
+# Procédure qui dessine les nouvelles lignes représentant les alignements
+def drawLines(mySurface,lines,player):
+
+
+# Procédure qui permet d'afficher le joueur gagnant
+def displayWinner(mySurface,n,scores):
+
+
+# Fonction qui permet au joueur de choisir une case et la lettre qu'il souhaite y mettre
+def selectSquare(mySurface,board,n):
+
+
+# Procédure qui gère le déroulement de la partie
+def gamePlay(mySurface,board,n,scores):
+
+
+# Procédure qui initialise et lance la partie
+def SOS(n):
