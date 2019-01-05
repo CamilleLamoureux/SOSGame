aa0acbbdac994d1df0beb23996149e3650b06d76
Camille <291756@supinfo.com>
2019-01-05 19:45:20 +0100

    Fin de DrawCell

--- sosGUI.py
+++ sosGUI.py
@@ -14,19 +14,19 @@ def drawBoard(mySurface,n):
     BLANC = (255, 255, 255)
     GRIS = (150,150,150)
 
-    x, y = 40, 100
+    x, y = 0,0
     for i in range(n + 1):
         pygame.draw.line(mySurface, BLANC, (x, y), (x + n * 75, y), 3)
         y += 75
         pygame.display.update()
 
-    x, y = 40, 100
+    x, y = 0,0
     for i in range(n + 1):
         pygame.draw.line(mySurface, BLANC, (x, y), (x, y + n * 75), 3)
         x += 75
         pygame.display.update()
 
-    x,y = 40,100
+    x,y = 0,0
     for ligne in range(n):
         for colonne in range(n):
             pygame.draw.line(mySurface, GRIS, (x + 37, y + 10), (x + 37, y + 65), 2)
@@ -41,7 +41,7 @@ def drawBoard(mySurface,n):
 
             x += 75
         y += 75
-        x = 40
+        x = 0
 
 
 # Procédure qui affiche les scores des joueurs
@@ -51,25 +51,52 @@ def drawBoard(mySurface,n):
 # Procédure qui permet d'afficher quel joueur doit jouer
 #def displayPlayer(mySurface,n,player):
 
-#Fonction qui retourne les coordonnées du coin en haut à gauche de la case cliquée
+# Fonction qui retourne les coordonnées du coin en haut à gauche de la case cliquée
 def case(i,j):
-    x = i - (i%75) + 40
+    x = i - (i%75)
     y = j - (j%75)
     return x,y
+
+
+# Fonction qui retourne la couleur dans laquelle doit être mis la lettre
+def colorPlayer(board):
+    nbr = 0
+    for colonne in board :
+        for element in colonne:
+            if element != 0:
+                nbr += 1
+
+    nombre_tours = n**2 - (nbr)
+
+    if nombre_tours%2 == 0:
+        return (255,0,0)
+    else:
+        return (0,255,0)
 
 # Procédure qui affiche la lettre posée par le joueur dont c'est le tour
 def drawCell(mySurface,board,i,j):
     x,y = case(i, j)
 
-    NOIR = (0,0,0)
+    font = pygame.font.Font(None, 100)
 
-    if (i-40)%75 <= 37:
+    GRIS_FOND = (50,50,50)
+    COULEUR_JOUEUR = colorPlayer(board)
+
+    if i%75 <= 37:
         print("S")
         l = 1
+        pygame.draw.rect(mySurface,GRIS_FOND,(x+2,y+2,72,72))
+        S = font.render("S", 1, COULEUR_JOUEUR)
+        mySurface.blit(S, (x + 15, y + 7))
+
     else :
         print("O")
         l = 2
+        pygame.draw.rect(mySurface,GRIS_FOND, (x+2, y+2, 72, 72))
+        O = font.render("O", 1, COULEUR_JOUEUR)
+        mySurface.blit(O, (x + 15, y + 7))
 
+    board[i//75][j//75] = l
     print(board)
 
 
@@ -92,7 +119,7 @@ def drawCell(mySurface,board,i,j):
 # Procédure qui initialise et lance la partie
 def SOS(n):
     pygame.init()
-    mySurface = pygame.display.set_mode((n*200, n*150))
+    mySurface = pygame.display.set_mode((n*75, n*75))
     pygame.display.set_caption('SOS Game')
     inProgress = True
 
