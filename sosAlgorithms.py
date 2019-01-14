bace5e362bf048126c52dbfbd45abcb48c836d83
Camille <291756@supinfo.com>
2019-01-14 19:51:43 +0100

    Globalisation des variables

--- sosAlgorithms.py
+++ sosAlgorithms.py
@@ -2,6 +2,11 @@
 # Anaïs Depeau et Camille Lamoureux
 # Bibliotheque des fonctions algorithmiques permettant le jeu
 
+# Déclaration des tailles
+LARGEUR_CASE = 75
+DEMI_CASE = 37
+MARGE_GAUCHE = 40
+MARGE_HAUT = 100
 
 # Fonction de creation d'un nouveau tableau
 def newboard(n):
@@ -23,80 +28,110 @@ def isintheboard(i, j, n):
 
 # Procedure qui met a jour lines et scores si on a pose un S
 def updateScoreS(board, n, i, j, scores, player, lines):
+
+    # Vérification des cases à gauche
     if isintheboard(i - 1, j, n) and board[i - 1][j] == 2 and isintheboard(i - 2, j, n) and board[i - 2][j] == 1:
-        scores[player] += 1
-        lines.append([[(i * 75 + 40 + 37), (j * 75 + 100 + 37)], \
-                      [((i - 2) * 75 + 40 + 37), (j * 75 + 100 + 37)]])
 
+        scores[player] += 1
+        lines.append([[(i * LARGEUR_CASE + MARGE_GAUCHE + DEMI_CASE), (j * LARGEUR_CASE + MARGE_HAUT + DEMI_CASE)], \
+                      [((i - 2) * LARGEUR_CASE + MARGE_GAUCHE + DEMI_CASE), (j * LARGEUR_CASE + MARGE_HAUT + DEMI_CASE)]])
+
+    # Vérification des cases à droite
     if isintheboard(i + 1, j, n) and board[i + 1][j] == 2 and isintheboard(i + 2, j, n) and board[i + 2][j] == 1:
-        scores[player] += 1
-        lines.append([[(i * 75 + 40 + 37), (j * 75 + 100 + 37)], \
-                      [((i + 2) * 75 + 40 + 37), (j * 75 + 100 + 37)]])
 
-    if isintheboard(i, j - 1, n) and board[i][j - 1] == 2 and \
-            isintheboard(i, j - 2, n) and board[i][j - 2] == 1:
         scores[player] += 1
-        lines.append([[(i * 75 + 40 + 37), (j * 75 + 100 + 37)], \
-                      [(i * 75 + 40 + 37), ((j - 2) * 75 + 100 + 37)]])
+        lines.append([[(i * LARGEUR_CASE + MARGE_GAUCHE + DEMI_CASE), (j * LARGEUR_CASE + MARGE_HAUT + DEMI_CASE)], \
+                      [((i + 2) * LARGEUR_CASE + MARGE_GAUCHE + DEMI_CASE), (j * LARGEUR_CASE + MARGE_HAUT + DEMI_CASE)]])
 
+    # Vérification des cases au dessus
+    if isintheboard(i, j - 1, n) and board[i][j - 1] == 2 and isintheboard(i, j - 2, n) and board[i][j - 2] == 1:
+
+        scores[player] += 1
+        lines.append([[(i * LARGEUR_CASE + MARGE_GAUCHE + DEMI_CASE), (j * LARGEUR_CASE + MARGE_HAUT+ DEMI_CASE)],\
+                      [(i * LARGEUR_CASE + MARGE_GAUCHE + DEMI_CASE), ((j - 2) * LARGEUR_CASE + MARGE_HAUT + DEMI_CASE)]])
+
+    # Vérification des cases en dessous
     if isintheboard(i, j + 1, n) and board[i][j + 1] == 2 and isintheboard(i, j + 2, n) and board[i][j + 2] == 1:
-        scores[player] += 1
-        lines.append([[(i * 75 + 40 + 37), (j * 75 + 100 + 37)],\
-                      [(i * 75 + 40 + 37), ((j + 2) * 75 + 100 + 37)]])
 
+        scores[player] += 1
+        lines.append([[(i * LARGEUR_CASE + MARGE_GAUCHE + DEMI_CASE), (j * LARGEUR_CASE + MARGE_HAUT + DEMI_CASE)],\
+                      [(i * LARGEUR_CASE + MARGE_GAUCHE + DEMI_CASE), ((j + 2) * LARGEUR_CASE + MARGE_HAUT + DEMI_CASE)]])
+
+    # Vérification des cases en diagonale en haut à gauche
     if isintheboard(i - 1, j - 1, n) and board[i - 1][j - 1] == 2 \
             and isintheboard(i - 2, j - 2, n) and board[i - 2][j - 2] == 1:
-        scores[player] += 1
-        lines.append([[(i * 75 + 40 + 37), (j * 75 + 100 + 37)],
-                      [((i - 2) * 75 + 40 + 37), ((j - 2) * 75 + 100 + 37)]])
 
+        scores[player] += 1
+        lines.append([[(i * LARGEUR_CASE + MARGE_GAUCHE + DEMI_CASE), (j * LARGEUR_CASE + MARGE_HAUT + DEMI_CASE)],
+                      [((i - 2) * LARGEUR_CASE + MARGE_GAUCHE + DEMI_CASE), ((j - 2) * LARGEUR_CASE + MARGE_HAUT + DEMI_CASE)]])
+
+    # Vérifiacation des cases en diagonale en bas à droite
     if isintheboard(i + 1, j + 1, n) and board[i + 1][j + 1] == 2 \
             and isintheboard(i + 2, j + 2, n) and board[i + 2][j + 2] == 1:
-        scores[player] += 1
-        lines.append([[(i * 75 + 40 + 37), (j * 75 + 100 + 37)],
-                      [((i + 2) * 75 + 40 + 37), ((j + 2) * 75 + 100 + 37)]])
 
+        scores[player] += 1
+        lines.append([[(i * LARGEUR_CASE + MARGE_GAUCHE + DEMI_CASE), (j * LARGEUR_CASE + MARGE_HAUT + DEMI_CASE)],
+                      [((i + 2) * LARGEUR_CASE + MARGE_GAUCHE + DEMI_CASE), ((j + 2) * LARGEUR_CASE + MARGE_HAUT + DEMI_CASE)]])
+
+    # Vérification des cases en digonale en haut à droite
     if isintheboard(i + 1, j - 1, n) and board[i + 1][j - 1] == 2 \
             and isintheboard(i + 2, j - 2, n) and board[i + 2][j - 2] == 1:
-        scores[player] += 1
-        lines.append([[(i * 75 + 40 + 37), (j * 75 + 100 + 37)],
-                      [((i + 2) * 75 + 40 + 37), ((j - 2) * 75 + 100 + 37)]])
 
+        scores[player] += 1
+        lines.append([[(i * LARGEUR_CASE + MARGE_GAUCHE + DEMI_CASE), (j * LARGEUR_CASE + MARGE_HAUT + DEMI_CASE)],
+                      [((i + 2) * LARGEUR_CASE + MARGE_GAUCHE + DEMI_CASE), ((j - 2) * LARGEUR_CASE + MARGE_HAUT + DEMI_CASE)]])
+
+    # Vérification des cases en diagonale en bas à gauche
     if isintheboard(i - 1, j + 1, n) and board[i - 1][j + 1] == 2 \
             and isintheboard(i - 2, j + 2, n) and board[i - 2][j + 2] == 1:
+
         scores[player] += 1
-        lines.append([[(i * 75 + 40 + 37), (j * 75 + 100 + 37)],
-                      [((i - 2) * 75 + 40 + 37), ((j + 2) * 75 + 100 + 37)]])
+        lines.append([[(i * LARGEUR_CASE + MARGE_GAUCHE + DEMI_CASE), (j * LARGEUR_CASE + MARGE_HAUT + DEMI_CASE)],
+                      [((i - 2) * LARGEUR_CASE + MARGE_GAUCHE + DEMI_CASE), ((j + 2) * LARGEUR_CASE + MARGE_HAUT + DEMI_CASE)]])
 
 
 # Procedure qui met a jour lines et scores si on a pose un O
 def updateScoreO(board, n, i, j, scores, player, lines):
+
+    # Vérification des cases à la verticale
     if isintheboard(i - 1, j, n) and board[i - 1][j] == 1 and isintheboard(i + 1, j, n) and board[i + 1][j] == 1:
-        scores[player] += 1
-        lines.append([[((i - 1) * 75 + 40 + 37), (j * 75 + 100 + 37)], [((i + 1) * 75 + 40 + 37), (j * 75 + 100 + 37)]])
 
+        scores[player] += 1
+        lines.append([[((i - 1) * LARGEUR_CASE + MARGE_GAUCHE + DEMI_CASE), (j * LARGEUR_CASE + MARGE_HAUT + DEMI_CASE)],\
+                      [((i + 1) * LARGEUR_CASE + MARGE_GAUCHE + DEMI_CASE), (j * LARGEUR_CASE + MARGE_HAUT + DEMI_CASE)]])
+
+    # Vérification des cases à l'horizontale
     if isintheboard(i, j - 1, n) and board[i][j - 1] == 1 and isintheboard(i, j + 1, n) and board[i][j + 1] == 1:
-        scores[player] += 1
-        lines.append([[(i * 75 + 40 + 37), ((j - 1) * 75 + 100 + 37)], [(i * 75 + 40 + 37), ((j + 1) * 75 + 100 + 37)]])
 
+        scores[player] += 1
+        lines.append([[(i * LARGEUR_CASE + MARGE_GAUCHE + DEMI_CASE), ((j - 1) * LARGEUR_CASE + MARGE_HAUT + DEMI_CASE)], \
+                      [(i * LARGEUR_CASE + MARGE_GAUCHE + DEMI_CASE), ((j + 1) * LARGEUR_CASE + MARGE_HAUT + DEMI_CASE)]])
+
+    # Vérification des cases en diagonale (de en haut à gauche à en bas à droite)
     if isintheboard(i - 1, j - 1, n) and board[i - 1][j - 1] == 1 \
             and isintheboard(i + 1, j + 1, n) and board[i + 1][j + 1] == 1:
-        scores[player] += 1
-        lines.append([[((i - 1) * 75 + 40 + 37), ((j - 1) * 75 + 100 + 37)],
-                      [((i + 1) * 75 + 40 + 37), ((j + 1) * 75 + 100 + 37)]])
 
+        scores[player] += 1
+        lines.append([[((i - 1) * LARGEUR_CASE + MARGE_GAUCHE + DEMI_CASE), ((j - 1) * LARGEUR_CASE + MARGE_HAUT + DEMI_CASE)],
+                      [((i + 1) * LARGEUR_CASE + MARGE_GAUCHE + DEMI_CASE), ((j + 1) * LARGEUR_CASE + MARGE_HAUT + DEMI_CASE)]])
+
+    # Vérification des cases en diagonale (de en bas à gauche à en haut à droite)
     if isintheboard(i - 1, j + 1, n) and board[i - 1][j + 1] == 1 \
             and isintheboard(i + 1, j - 1, n) and board[i + 1][j - 1] == 1:
+
         scores[player] += 1
-        lines.append([[((i - 1) * 75 + 40 + 37), ((j + 1) * 75 + 100 + 37)],
-                      [((i + 1) * 75 + 40 + 37), ((j - 1) * 75 + 100 + 37)]])
+        lines.append([[((i - 1) * LARGEUR_CASE + MARGE_GAUCHE + DEMI_CASE), ((j + 1) * LARGEUR_CASE + MARGE_HAUT + DEMI_CASE)],
+                      [((i + 1) * LARGEUR_CASE + MARGE_GAUCHE + DEMI_CASE), ((j - 1) * LARGEUR_CASE + MARGE_HAUT + DEMI_CASE)]])
 
 
 # Prodecudre qui met a jour le plateau de jeu
 # AJOUTER SCORES et LINES en variables
 def update(board, n, i, j, l, scores, player, lines):
+
+    # Mise à jour du tableau
     board[i][j] = l
 
+    # Appel de la procédure associée à la lettre choisie
     if l == 1:
         updateScoreS(board, n, i, j, scores, player, lines)
     elif l == 2:
@@ -106,8 +141,8 @@ def update(board, n, i, j, l, scores, player, lines):
 # Fonction qui retourne le gagnant de la partie
 def winner(scores):
     if scores[0] > scores[1]:
-        return "Player 1 wins !"
+        return "Le joueur 1 gagne la partie !"
     elif scores[0] == scores[1]:
-        return "Egality"
+        return "Égalité..."
     else:
-        return "Player 2 wins !"
+        return "Le joueur 2 gagne la partie !"
--- sosGUI.py
+++ sosGUI.py
@@ -10,86 +10,107 @@ import sosAlgorithms
 
 
 ############################################################# GRAPHISME ###########################################################
+# Déclaration des couleurs
+BLANC = (255, 255, 255)
+MARRON_CLAIR = (168, 125, 125)
+BLEU = (28, 95, 119)
+VIOLET = (87, 25, 83)
+BLEU_FOND = (186, 207, 214)
+VIOLET_FOND = (204, 186, 203)
+COULEUR_DE_FOND = (188, 174, 174)
+
+# Déclaration des tailles
+LARGEUR_CASE = 75
+DEMI_CASE = 37
+MARGE_GAUCHE = 40
+MARGE_HAUT = 100
+
 # Procédure qui dessine le tableau initial
 def drawBoard(mySurface, n):
-    BLANC = (255, 255, 255)
-    MARRON_CLAIR = (168, 125, 125)
-
+    # Déclaration de la police
     font = pygame.font.Font(None, 30)
 
+    # Déclaration des lettres
     S = font.render("S", 1, MARRON_CLAIR)
     O = font.render("O", 1, MARRON_CLAIR)
 
+    # Affichage du tableau
     x, y = 40, 100
     for i in range(n + 1):
-        pygame.draw.line(mySurface, BLANC, (x, y), (x + n * 75, y), 3)
-        y += 75
+        pygame.draw.line(mySurface, BLANC, (x, y), (x + n * LARGEUR_CASE, y), 3)
+        y += LARGEUR_CASE
         pygame.display.update()
 
     x, y = 40, 100
     for i in range(n + 1):
-        pygame.draw.line(mySurface, BLANC, (x, y), (x, y + n * 75), 3)
-        x += 75
+        pygame.draw.line(mySurface, BLANC, (x, y), (x, y + n * LARGEUR_CASE), 3)
+        x += LARGEUR_CASE
         pygame.display.update()
 
     x, y = 40, 100
     for ligne in range(n):
         for colonne in range(n):
-            pygame.draw.line(mySurface, MARRON_CLAIR, (x + 37, y + 10), (x + 37, y + 65), 2)
+            pygame.draw.line(mySurface, MARRON_CLAIR, (x + DEMI_CASE, y + 10), (x + DEMI_CASE, y + 65), 2)
 
             mySurface.blit(S, (x + 10, y + 35))
             mySurface.blit(O, (x + 47, y + 35))
 
-            x += 75
-        y += 75
+            x += LARGEUR_CASE
+        y += LARGEUR_CASE
         x = 40
 
 
 # Procédure qui affiche les scores des joueurs
 def displayScore(mySurface, n, scores):
-    BLEU = (28, 95, 119)
-    VIOLET = (87, 25, 83)
-    MARRON_CLAIR = (168, 125, 125)
-
+    # Déclaration des polices
     font = pygame.font.Font(None, 46)
     font_joueur = pygame.font.Font(None, 33)
+
+    # Déclaration  du titre
     affichage_scores = font.render("Scores :", 1, (MARRON_CLAIR))
+
+    # Délcarations des points du joueur 1
     joueur1 = font_joueur.render("Joueur 1 :", 1, (BLEU))
-    joueur2 = font_joueur.render("Joueur 2 :", 1, (VIOLET))
     pts_joueur_1 = font_joueur.render(str(scores[0]), 1, (BLEU))
+
+    # Délacarations des points du joueur 2
+    joueur2 = font_joueur.render("Joueur 2 :", 1, (VIOLET))
     pts_joueur_2 = font_joueur.render(str(scores[1]), 1, (VIOLET))
 
-    pygame.draw.rect(mySurface, (188, 174, 174), (40 + n * 75 + 205, 190, 75, 100))
+    # Affichage du titre et des points des joueurs
+    pygame.draw.rect(mySurface, (188, 174, 174), (MARGE_GAUCHE + n * LARGEUR_CASE + 205, 190, LARGEUR_CASE, 100))
 
-    mySurface.blit(affichage_scores, (40 + n * 75 + 60, 140))
-    mySurface.blit(joueur1, (40 + n * 75 + 100, 190))
-    mySurface.blit(pts_joueur_1, (40 + n * 75 + 215, 190))
-    mySurface.blit(joueur2, (40 + n * 75 + 100, 220))
-    mySurface.blit(pts_joueur_2, (40 + n * 75 + 215, 220))
+    mySurface.blit(affichage_scores, (MARGE_GAUCHE + n * LARGEUR_CASE + 60, 140))
+
+    mySurface.blit(joueur1, (MARGE_GAUCHE + n * LARGEUR_CASE + MARGE_HAUT, 190))
+    mySurface.blit(pts_joueur_1, (MARGE_GAUCHE + n * LARGEUR_CASE + 215, 190))
+
+    mySurface.blit(joueur2, (MARGE_GAUCHE + n * LARGEUR_CASE + MARGE_HAUT, 220))
+    mySurface.blit(pts_joueur_2, (MARGE_GAUCHE + n * LARGEUR_CASE + 215, 220))
 
 
 # Procédure qui permet d'afficher quel joueur doit jouer
 def displayPlayer(mySurface, n, player):
-    pygame.draw.rect(mySurface, (188, 174, 174), (40 + n * 75 + 55, 180, 30, 100))
+    pygame.draw.rect(mySurface, (188, 174, 174), (MARGE_GAUCHE + n * LARGEUR_CASE + 55, 180, 30, MARGE_HAUT))
 
     fleche = pygame.image.load("flechejoueur.gif")
 
     if player == 0:
 
-        mySurface.blit(fleche, (40 + n * 75 + 55, 195))
+        mySurface.blit(fleche, (MARGE_GAUCHE + n * LARGEUR_CASE + 55, 195))
     else:
 
-        mySurface.blit(fleche, (40 + n * 75 + 55, 220))
+        mySurface.blit(fleche, (MARGE_GAUCHE + n * LARGEUR_CASE + 55, 220))
 
 
 # Fonction qui retourne les coordonnées du coin en haut à gauche de la case cliquée
 def case(i, j):
-    i -= 40
-    j -= 100
-    x = i - (i % 75)
-    y = j - (j % 75)
-    x = x // 75
-    y = y // 75
+    i -= MARGE_GAUCHE
+    j -= MARGE_HAUT
+    x = i - (i % LARGEUR_CASE)
+    y = j - (j % LARGEUR_CASE)
+    x = x // LARGEUR_CASE
+    y = y // LARGEUR_CASE
     return x, y
 
 
@@ -97,62 +118,44 @@ def case(i, j):
 def drawCell(mySurface, board, i, j, player):
     font = pygame.font.Font(None, 100)
 
-    BLEU = (28, 95, 119)
-    VIOLET = (87, 25, 83)
-    BLEU_FOND = (186, 207, 214)
-    VIOLET_FOND = (204, 186, 203)
-
+    # Détermination des couleurs à utiliser
     COULEUR_JOUEUR = BLEU if player == 0 else VIOLET
     COULEUR_FOND = BLEU_FOND if player == 0 else VIOLET_FOND
 
+    # Déclaration des lettres
     S = font.render("S", 1, COULEUR_JOUEUR)
     O = font.render("O", 1, COULEUR_JOUEUR)
 
+    # Récupération de la lettre choisie
     l = board[i][j]
 
-    pygame.draw.rect(mySurface, COULEUR_FOND, (i * 75 + 2 + 40, j * 75 + 2 + 100, 72, 72))
-
+    # Affichage de la lettre choisie
+    pygame.draw.rect(mySurface, COULEUR_FOND, (i * LARGEUR_CASE + 2 + MARGE_GAUCHE, j * LARGEUR_CASE + 2 + MARGE_HAUT, 72, 72))
     if l == 1:
-        mySurface.blit(S, (i * 75 + 15 + 40, j * 75 + 7 + 100))
+        mySurface.blit(S, (i * LARGEUR_CASE + 15 + MARGE_GAUCHE, j * LARGEUR_CASE + 7 + MARGE_HAUT))
     elif l == 2:
-        mySurface.blit(O, (i * 75 + 15 + 40, j * 75 + 7 + 100))
+        mySurface.blit(O, (i * LARGEUR_CASE + 12 + MARGE_GAUCHE, j * LARGEUR_CASE + 7 + MARGE_HAUT))
 
 
 # Procédure qui dessine les nouvelles lignes représentant les alignements
 def drawLines(mySurface, lines, player):
-    BLEU = (8, 75, 99)
-    VIOLET = (67, 5, 63)
 
+    # Détermination de la couleur à utiliser
     COULEUR_JOUEUR = BLEU if player == 0 else VIOLET
 
+    # Affichage des lignes
     for ligne in lines:
         pygame.draw.line(mySurface, COULEUR_JOUEUR, ligne[0], ligne[1], 5)
 
 
 # Procédure qui permet d'afficher le joueur gagnant
-"""def displayWinner(mySurface, n, scores):
-    player = 0
-    player0 = player
-    player1 = 1
-    font = pygame.font.Font(None, 33)
-
-    if scores[player0] > scores[player1]:
-        gagnant = font.render("Le  joueur 1  a gagné la partie !", 1, (132,46,27))
-        mySurface.blit(gagnant, (40 + n * 75 + 45, 290))
-    else:
-        gagnant = font.render("Le  joueur 2  a gagné la partie !", 1, (157,62,12))
-        mySurface.blit(gagnant, (40 + n * 75 + 45, 290))"""
-
-
 def displayWinner(mySurface, n, scores):
-    font = pygame.font.Font(None, 33)
-    
-    #Soit
-    winner = sosAlgorithms.winner(scores)
-    gagnant = font.render(winner,1,(132,46,47))
-    #Soit
-    gagnant = font.render(sosAlgorithms.winner(scores),1,(132,46,47))
 
+    # Déclaration de la police
+    font = pygame.font.Font(None, 33)
+
+    # Affichage du joueur gagnant
+    gagnant = font.render(sosAlgorithms.winner(scores),1,(132,46,47))
     mySurface.blit(gagnant, (40 + n * 75 + 45, 290))
 
 
@@ -161,28 +164,11 @@ def displayWinner(mySurface, n, scores):
 
 
 ############################################################### JEU ##################################################################
-
-# Procédure qui initialise et lance la partie
-def SOS(n):
-    pygame.init()
-    HAUTEUR = n * 75 + 140
-    LARGEUR = n * 75 + 440
-    MARRON_CLAIR = (168, 125, 125)
-
-    font = pygame.font.Font(None, 100)
-    titre = font.render("SOS Game!", 1, MARRON_CLAIR)
-
-    mySurface = pygame.display.set_mode((LARGEUR, HAUTEUR))
-    pygame.display.set_caption('SOS Game')
-    inProgress = True
-
-    mySurface.fill((188, 174, 174))
-    mySurface.blit(titre, ((LARGEUR // 4), 20))
-    board = sosAlgorithms.newboard(n)
-    drawBoard(mySurface, n)
+# Procédure qui gère la partie
+def gameplay(mySurface, board, n, scores):
 
     player = 0
-    scores = [0, 0]
+    inProgress = True
 
     while inProgress:
         for event in pygame.event.get():
@@ -231,7 +217,36 @@ def SOS(n):
             displayWinner(mySurface, n, scores)
 
     pygame.quit()
+# Procédure qui initialise et lance la partie
+def SOS(n):
+    pygame.init()
 
+    # Déclaration du titre de la fenêtre
+    font = pygame.font.Font(None, 70)
+    titre = font.render("SOS GAME "*n, 1, MARRON_CLAIR)
+
+    # Déclaration de la taille de la fenêtre
+    HAUTEUR = n * 75 + 140
+    LARGEUR = n * 75 + 440
+
+    # Création de la fenêtre
+    mySurface = pygame.display.set_mode((LARGEUR, HAUTEUR))
+    pygame.display.set_caption('SOS Game')
+
+    # Affichage du design choisit
+    mySurface.fill(COULEUR_DE_FOND)
+    mySurface.blit(titre, (0,20))
+
+    # Création du tableau
+    board = sosAlgorithms.newboard(n)
+
+    # Afichage du tableau
+    drawBoard(mySurface, n)
+
+    # Initialisation des scores
+    scores = [0, 0]
+
+    gameplay(mySurface, board, n, scores)
 
 # Appel de la procédure permettant de lancer le jeu
 n = int(input("Choisissez la taille de votre tableau de jeu : "))
